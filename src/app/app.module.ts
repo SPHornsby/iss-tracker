fb5d214813abef6f16941a9faa79837c80c08e35
Sean Hornsby <sean.hornsby@gmail.com>
2017-05-04 07:26:55 -0700

    Adds jsonp.
    Inlines map functors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DataService } from './data.service';
@@ -19,7 +19,8 @@ import { CurrentComponent } from './current/current.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    JsonpModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,46 +1,36 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Jsonp } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
 import { Astronaut, NextPasses, CurrentLocation } from './app.component';
 
 @Injectable()
 export class DataService {
-
-  private peopleUrl = 'http://api.open-notify.org/astros.json';  // URL to get current ISS personnel
-  private passesUrl = 'http://api.open-notify.org/iss-pass.json?lat=45.0&lon=-122.3&alt=20' // URL to get ISS passes from lat/long
-  constructor(private http: Http) { }
-  private locationUrl = 'http://api.open-notify.org/iss-now.json' // URL to get current ISS location (overhead)
-
+  // URL to get current ISS personnel
+  private peopleUrl = 'http://api.open-notify.org/astros.json';
+  // URL to get ISS passes from lat/long
+  private passesUrl = 'http://api.open-notify.org/iss-pass.json?lat=45.0&lon=-122.3&alt=20&callback=JSONP_CALLBACK';
+  // URL to get current ISS location (overhead)
+  private locationUrl = 'http://api.open-notify.org/iss-now.json';
+  
+  constructor(private http: Http, private jsonp: Jsonp) { }
+  
   getPeople(): Observable<Astronaut[]> {
     return this.http.get(this.peopleUrl)
-                    .map(this.extractPeople)
+                    .map((res:Response) => res.json().people)
                     .catch(this.handleError);
   }
   getPasses(): Observable<NextPasses[]> {
-    return this.http.get(this.passesUrl)
-               .map(this.extractPasses)
+    return this.jsonp.get(this.passesUrl)
+               .map((res:Response) => res.json().response)
                .catch(this.handleError);
   }
   getCurrentPosition(): Observable<CurrentLocation> {
     return this.http.get(this.locationUrl)
-               .map(this.extractCurrent)
+               .map((res:Response) => res.json())
                .catch(this.handleError);
-  }
-  private extractPeople(res: Response) {  
-    let body = res.json();
-    console.log(body);
-    return body.people || { };
-  }
-  private extractPasses(res: Response) {  
-    let body = res.json();
-    return body.responses || { };
-  }
-  private extractCurrent(res: Response) {  
-    let body = res.json();
-    console.log(body);
-    return body || { };
   }
   private handleError (error: Response | any) {
     let errMsg: string;
@@ -51,7 +41,6 @@ export class DataService {
     } else {
       errMsg = error.message ? error.message : error.toString();
     }
-    console.error(errMsg);
     return Observable.throw(errMsg);
   }
 }
