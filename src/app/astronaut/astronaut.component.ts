040b3f6cb67e5a079a6285c9fb87ccc2d3eacd54
Sean Hornsby <sean.hornsby@gmail.com>
2017-05-04 13:59:17 -0700

    Parse astronaut names for wiki links

--- src/app/astronaut/astronaut.component.ts
+++ src/app/astronaut/astronaut.component.ts
@@ -11,7 +11,7 @@ export class AstronautComponent implements OnInit {
 
   errorMessage: string;
   astronauts: Astronaut[];
-
+  wikiTerms: string[];
   constructor(private dataService:DataService) { }
 
   ngOnInit() {
@@ -21,8 +21,17 @@ export class AstronautComponent implements OnInit {
   getAstronauts() {
     this.dataService.getPeople()
                     .subscribe(
-                      astronauts => this.astronauts = astronauts,
+                      astronauts => {
+                        this.doWikiStuff(astronauts)
+                        return this.astronauts = astronauts}
+                        ,
                       error => this.errorMessage = <any>error
                     )
+                    
+  }
+  doWikiStuff(stuff) {
+    this.wikiTerms = stuff.map((astro) => {
+      return {term:astro.name.replace(" ", "_"), name: astro.name};
+    })
   }
 }
