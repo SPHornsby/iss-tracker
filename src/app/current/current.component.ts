8f02449fa63be79bc37b21432db068f807f72129
Sean Hornsby <sean.hornsby@gmail.com>
2017-05-03 17:47:33 -0700

    Add current location component

---
+++ src/app/current/current.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { CurrentLocation } from '../app.component';
+
+@Component({
+  selector: 'app-current',
+  templateUrl: './current.component.html',
+  styleUrls: ['./current.component.css']
+})
+export class CurrentComponent implements OnInit {
+
+  errorMessage: string;
+  currentLocation: CurrentLocation;
+  latitude: string;
+  longitude: string;
+  timestamp: Date;
+  
+  constructor(private dataService:DataService) { }
+
+  ngOnInit() {
+    this.getCurrent();
+  }
+
+  getCurrent() {
+    this.dataService.getCurrentPosition()
+                    .subscribe(
+                      (currentLocation) => {
+                        console.log("cl",currentLocation);
+                        this.longitude = currentLocation.iss_position.longitude;
+                        this.latitude = currentLocation.iss_position.latitude;
+                        this.timestamp = new Date(currentLocation.timestamp * 1000);
+                      },
+                      error => this.errorMessage = <any>error
+                    )
+  }
+}
