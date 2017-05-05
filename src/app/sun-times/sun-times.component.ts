d616363fdc09d7059c162fd1cb261be2bb3a779d
Sean Hornsby <sean.hornsby@gmail.com>
2017-05-04 17:10:29 -0700

    Call SunTimes data to fill component

--- src/app/sun-times/sun-times.component.ts
+++ src/app/sun-times/sun-times.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { SunTimes } from '../app.component';
 
 @Component({
   selector: 'app-sun-times',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SunTimesComponent implements OnInit {
 
-  constructor() { }
+  sunTimes: SunTimes;
+  latitude: string;
+  longitude: string;
+  errorMessage: string;
+  sunrise: string;
+  sunset: string;
+  dayLength: string;
+  constructor(private dataService:DataService) { }
 
   ngOnInit() {
+    
   }
-
+  getSunTimes() {
+    this.dataService.getSunTimes(this.latitude, this.longitude)
+                    .subscribe(
+                      sunTimes => this.mapToValues(sunTimes),
+                      error => this.errorMessage = <any>error
+                    )
+  }
+  setLocation(lat, lon) {
+    console.log(lat, lon);
+    this.latitude = lat;
+    this.longitude = lon;
+    this.getSunTimes();
+  }
+  mapToValues(sunTimes) {
+    this.sunrise = new Date(Date.parse(sunTimes.sunrise)).toString();
+    this.sunset = new Date(Date.parse(sunTimes.sunset)).toString();
+    this.dayLength = sunTimes.day_length;
+  }
 }
